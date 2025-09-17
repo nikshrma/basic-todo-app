3fbeccab3e533a3db878f08ff464562b20e197de
nikshrma <nikshrma2006@gmail.com>
2025-09-18 01:00:04 +0530

    Finished front-end

--- front-end/src/components/CreateTodo.jsx
+++ front-end/src/components/CreateTodo.jsx
@@ -1,7 +1,28 @@
-export function CreateTodo(){
-    return <div>
-        <input type="text" placeholder="Title"></input><br/>
-        <input type="text" placeholder="Description"></input><br/>
-        <button>Add a todo</button>
-    </div>
+import { useState } from "react";
+export function CreateTodo() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    return (<div>
+        <input type="text" placeholder="Title" onChange={(e) => {
+            const value = e.target.value;
+            setTitle(value);
+        }}></input><br />
+        <input type="text" placeholder="Description" onChange={(e) => {
+            const value = e.target.value;
+            setDescription(value);
+        }}></input><br />
+        <button onClick={() => {
+            fetch("http://localhost:3000/todos", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+            headers:{
+                "Content-Type": "application/json"
+            }
+        });
+        alert("added todo");
+    }}>Add a todo</button>
+    </div>);
 }
