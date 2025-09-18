ea697f8f4b5b3c12716ba38c1cd5c4f31b939670
nikshrma <nikshrma2006@gmail.com>
2025-09-18 23:33:21 +0530

    Used the useEffect hook

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 
 function App() {
-const[todos,setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-fetch("http://localhost:3000/todos")
-.then(async(res)=>{
-  const json = await res.json();
-  setTodos(json.todos);
-})
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
+      .then(async (res) => {
+        const json = await res.json();
+        setTodos(json.todos);
+      });
+  }, []); 
+
   return (
     <div>
-      <CreateTodo></CreateTodo>
-      <Todos todos = {todos}></Todos>
+      <CreateTodo setTodos={setTodos} />
+      <Todos todos={todos} setTodos={setTodos} />
     </div>
   )
 }
