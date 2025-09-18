60fd20fc76588570b640ab0dc044097f107c1479
nikshrma <nikshrma2006@gmail.com>
2025-09-18 23:34:09 +0530

    Added functionality to mark todo as done

--- front-end/src/components/Todos.jsx
+++ front-end/src/components/Todos.jsx
@@ -1,10 +1,31 @@
-export function Todos({todos}){
+function handleCompleted(id,todos,setTodos){
+    fetch("http://localhost:3000/completed",{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            id
+        })
+    })
+    .then (async(res)=>{
+        const modifiedTodo = await res.json();
+        const newTodos = todos.map((t) =>
+        t._id === modifiedTodo.updatedTodo._id ? modifiedTodo.updatedTodo : t
+      );
+
+      setTodos(newTodos);
+    })
+}
+export function Todos({todos , setTodos}){
     return <div>
         {todos.map((todo)=>{
             return <div>
                <h1>{todo.title}</h1>
                <h1>{todo.description}</h1>
-               <button>{todo.completed==true?"Completed" : "Mark as completed"}</button>
+               {todo.completed?<span>Completed</span>:<button onClick={()=>{
+                handleCompleted(todo._id,todos ,setTodos);
+               }}>Mark as done</button>}
                 </div>
         })}
     </div>
